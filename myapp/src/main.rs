adda188976ecc52bee9670cf8ecab66375445668
sukrozt <sukriyeo13@gmail.com>
2025-09-01 16:33:34 +0300

    init

---
+++ myapp/src/main.rs
@@ -0,0 +1,43 @@
+use anyhow::Context;
+use aya::programs::{Xdp, XdpFlags};
+use aya_log::EbpfLogger;
+use clap::Parser;
+use log::info;
+use tokio::signal; // 
+
+#[derive(Debug, Parser)]
+struct Opt {
+    #[clap(short, long, default_value = "ens33")]
+    iface: String, // 
+}
+
+#[tokio::main] // 
+async fn main() -> Result<(), anyhow::Error> {
+    let opt = Opt::parse();
+
+    env_logger::init();
+
+    // This will include your eBPF object file as raw bytes at compile-time and load it at
+    // runtime. This approach is recommended for most real-world use cases. If you would
+    // like to specify the eBPF program at runtime rather than at compile-time, you can
+    // reach for `Ebpf::load_file` instead.
+    // 
+    // 
+    let mut bpf = aya::Ebpf::load(aya::include_bytes_aligned!(concat!(
+        env!("OUT_DIR"),
+        "/myapp"
+    )))?;
+    EbpfLogger::init(&mut bpf)?;
+    // 
+    let program: &mut Xdp = bpf.program_mut("myapp").unwrap().try_into()?;
+    program.load()?; // 
+                     // 
+    program.attach(&opt.iface, XdpFlags::default())
+        .context("failed to attach the XDP program with default flags - try changing XdpFlags::default() to XdpFlags::SKB_MODE")?;
+
+    info!("Waiting for Ctrl-C...");
+    signal::ctrl_c().await?;
+    info!("Exiting...");
+
+    Ok(())
+}
